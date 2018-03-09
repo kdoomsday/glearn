a18a42da453d5a84a84c53e0ffdeb51037a03af5
g33k09 <30420087+g33k09@users.noreply.github.com>
2018-03-08 22:08:26 -0400

    Arregla #1 y #3
    Se cambia la forma de recorrer la lista a un foreach y se arregla la documentación

--- src/main/java/geometria/AreaTotal.java
+++ src/main/java/geometria/AreaTotal.java
@@ -1,17 +1,19 @@
 package geometria;
 
+import java.util.List;
+
 /**
  * Calcula el Area Total de una lista de Figuras
+ * 
  * @author g33k09
  * 
  */
-import java.util.List;
 
 public class AreaTotal {
 	public static double areaTotal(List<FiguraGeometrica> sum) {
 		double suma = 0;
-		for (int i = 0; i < sum.size(); i++) {
-			suma = suma + sum.get(i).area();
+		for (FiguraGeometrica area : sum) {
+			suma = suma + area.area();
 		}
 		return suma;
 
